55ce3e445d971472e370ffac9b7f1762a41266c5
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-06-11 15:35:10 +0100

    Initial state, including prebuilt sample packages

---
+++ StandaloneApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.AspNetCore.Blazor.Electron;
+
+namespace StandaloneApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Launcher.StartElectronProcess(async () =>
+            {
+                var window = await Launcher.CreateWindowAsync("./wwwroot/index.html");
+
+                var serviceProvider = new BrowserServiceProvider(configure =>
+                {
+                    // ... register services here
+                });
+
+                new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+            });
+        }
+    }
+}
